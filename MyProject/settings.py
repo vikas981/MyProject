d26aa6933ae3acae1a470844d1d921a820182cdb
vikas981 <vs98990@gmail.com>
2020-09-15 23:20:08 +0530

    third commit

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'MyProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':  'ddbuk00en6o17t',
+        'USER': 'prpkzdwdlylcmz',
+        'PASSWORD': 'aacffd5e3d6aee7ba4142a3e1d2fe83e4d1a892224d7bfc281ff22c5bb76ac65',
+        'HOST': 'postgres://prpkzdwdlylcmz:aacffd5e3d6aee7ba4142a3e1d2fe83e4d1a892224d7bfc281ff22c5bb76ac65@ec2-54-157-234-29.compute-1.amazonaws.com:5432/ddbuk00en6o17t',
+        'PORT': '5432',
     }
 }
 
